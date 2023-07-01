ce1d6c433429ce593de5cf6dc73dc7dbba3ccb70
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-07-01 14:51:14 +0530

    document the code

--- sieve_of_eratosthenes/sieve_of_eratosthenes.go
+++ sieve_of_eratosthenes/sieve_of_eratosthenes.go
@@ -6,20 +6,31 @@ import (
 )
 
 func main() {
+	// Calling the getPrimeNumbers function with input 23
 	primeNumbers := getPrimeNumbers(100)
+
+	// Printing the list of prime numbers
 	fmt.Println("Prime Numbers are: ")
 	fmt.Println(primeNumbers)
+
+	// Printing the count of prime numbers
 	fmt.Println("Count: ", len(primeNumbers))
 }
 
+// getPrimeNumbers generates a list of prime numbers from range [0,n] using Sieve of Eratosthenes algorithm.
+// It takes in an integer n as input and returns a slice of prime numbers.
 func getPrimeNumbers(n int) []int {
-
+	// Creating a list of numbers from 0 to n
 	numbersList := []int{}
-
 	for i := 0; i <= n; i++ {
 		numbersList = append(numbersList, i)
 	}
+
+	// Finding the square root of n for optimization
 	root := int(math.Sqrt(float64(n)))
+
+	// Sieve of Eratosthenes algorithm
+	// Marking multiples of numbers as -1
 	for i := 2; i <= root; i++ {
 		for j := i + 1; j <= n; j++ {
 			currentNumber := numbersList[j]
@@ -32,18 +43,16 @@ func getPrimeNumbers(n int) []int {
 		}
 	}
 
+	// Extracting prime numbers from the list
 	primeNumbersList := []int{}
-
 	for i := 0; i <= n; i++ {
 		currentNumber := numbersList[i]
-
 		if currentNumber != -1 {
-			primeNumbersList =
-				append(primeNumbersList, currentNumber)
+			primeNumbersList = append(primeNumbersList, currentNumber)
 		}
 	}
 
-	// Since 0 & 1 are not prime numbers
+	// Since 0 and 1 are not prime numbers, removing them from the list
 	primeNumbersList = primeNumbersList[2:]
 
 	return primeNumbersList
