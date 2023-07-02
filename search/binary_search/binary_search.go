f60608de6de10320825d5b5b1582b6a72dfa0c5c
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-07-02 13:50:24 +0530

    add docs

--- search/binary_search/binary_search.go
+++ search/binary_search/binary_search.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// Numbers should be sorted in ascending order
 	numbers := []int{1, 2, 5, 8, 10, 12, 90}
 
 	index := binarySearch(numbers, 1)
@@ -15,23 +16,23 @@ func main() {
 }
 
 // binarySearch performs a binary search algorithm to find the index of the key element in the given slice of numbers.
-func binarySearch(numbers []int, key int) int {
-	n := len(numbers)
+func binarySearch(sortedNumbers []int, key int) int {
+	n := len(sortedNumbers)
 
 	low := 0
 	high := n
 
 	// Call binarySearchNonRecursive to perform a non-recursive binary search
-	index := binarySearchNonRecursive(numbers, low, high, key)
+	index := binarySearchNonRecursive(sortedNumbers, low, high, key)
 
 	// Call binarySearchRecursive to perform a recursive binary search
-	index = binarySearchRecursive(numbers, low, high, key)
+	index = binarySearchRecursive(sortedNumbers, low, high, key)
 
 	return index
 }
 
 // binarySearchNonRecursive performs a non-recursive binary search on the given slice of numbers.
-func binarySearchNonRecursive(numbers []int, low int, high int, key int) int {
+func binarySearchNonRecursive(sortedNumbers []int, low int, high int, key int) int {
 	lowIndex := low
 	highIndex := high
 
@@ -39,11 +40,11 @@ func binarySearchNonRecursive(numbers []int, low int, high int, key int) int {
 		mid := (lowIndex + highIndex) / 2
 
 		// Check if the mid element is equal to the key
-		if key == numbers[mid] {
+		if key == sortedNumbers[mid] {
 			return mid
 			// If key is greater than mid element, then key is to the right half of the array
 			// hence divide the array by altering the lower index
-		} else if key > numbers[mid] {
+		} else if key > sortedNumbers[mid] {
 			lowIndex = mid + 1
 			// Here, key is less than the mid element, hence the key is to the left half of the array
 		} else {
@@ -56,25 +57,25 @@ func binarySearchNonRecursive(numbers []int, low int, high int, key int) int {
 }
 
 // binarySearchRecursive performs a recursive binary search on the given slice of numbers.
-func binarySearchRecursive(numbers []int, low int, high int, key int) int {
+func binarySearchRecursive(sortedNumbers []int, low int, high int, key int) int {
 	mid := (low + high) / 2
 
 	// Check if the lower index is less than the higher index
 	if low < high {
 		// Check if the mid element is equal to the key
-		if numbers[mid] == key {
+		if sortedNumbers[mid] == key {
 			return mid
 			// If key is greater than mid element, then key is to the right half of the array
 			// hence divide the array by altering the lower index
-		} else if key > numbers[mid] {
-			return binarySearchRecursive(numbers, mid+1, high, key)
+		} else if key > sortedNumbers[mid] {
+			return binarySearchRecursive(sortedNumbers, mid+1, high, key)
 			// Here, key is less than the mid element, hence the key is to the left half of the array
 		} else {
-			return binarySearchRecursive(numbers, low, mid-1, key)
+			return binarySearchRecursive(sortedNumbers, low, mid-1, key)
 		}
 	} else {
 		// This case reaches if the key is at the ends of the array & when program reaches to the last recursion or smallest array of size 1
-		if numbers[mid] == key {
+		if sortedNumbers[mid] == key {
 			return mid
 		} else {
 			return -1
