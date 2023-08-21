c6e7f9c29ed8e708dcb90aa161b1d15d43789f59
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-08-21 22:58:43 +0530

    remove comments

--- leetcode/26_remove_duplicates.go
+++ leetcode/26_remove_duplicates.go
@@ -1,32 +1,27 @@
 package main
 
 func RemoveDuplicates(nums []int) int {
-	// Get the length of the input slice
+
 	n := len(nums)
 
-	// Create a map to store non-duplicate numbers
-	nonDuplicates := make(map[int]int)
+	visitedNumbers := make(map[int]int)
 
-	// Initialize variables
 	var currentNumber int
 	newIndex := 0
 
-	// Iterate over the input slice
 	for i := 0; i < n; i++ {
 		currentNumber = nums[i]
 
-		// Check if the current number is already in the nonDuplicates map
-		if nonDuplicates[currentNumber] > 0 {
-			// If it is, continue to the next iteration without updating the slice or map
+		if visitedNumbers[currentNumber] > 0 {
+
 			continue
 		} else {
-			// If it's not, update the slice and map with the current number
+
 			nums[newIndex] = currentNumber
-			nonDuplicates[currentNumber] = 1
+			visitedNumbers[currentNumber] = 1
 			newIndex++
 		}
 	}
 
-	// Return the new length of the slice after removing duplicates
 	return newIndex
 }
