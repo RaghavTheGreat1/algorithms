feba1701f53498316c81149e6d2fd8e19eb07713
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-08-23 00:57:43 +0530

    2011

---
+++ leetcode/2011_.operation.go
@@ -0,0 +1,13 @@
+package main
+
+func FinalValueAfterOperations(operations []string) int {
+	x:= 0
+	for _,v := range operations{
+		if v[0:2] == "++" || v[1:] == "++"{
+			x++
+		}else{
+			x--
+		}
+	}
+	return x
+}
