af363be18bbad79971cd9ded7b3033990d7cf809
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-08-23 00:05:23 +0530

    121_best time to buy

---
+++ leetcode/121_best_time_to_buy.go
@@ -0,0 +1,25 @@
+package main
+
+func MaxProfit(prices []int) int {
+	minIndex, minValue := 0,prices[0]
+	profit := 0
+
+	for i,v := range prices{
+		if v < minValue{
+			minIndex, minValue = i, v
+		}
+
+		if i > minIndex{
+			tempProfit := v - minValue
+			if tempProfit > profit{
+				profit = tempProfit
+			}
+		}
+	}
+
+	if profit < 0{
+		return 0
+	}
+
+	return profit
+}
