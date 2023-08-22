311a6c90164677c0a107a3bcb5d145d6b42837b9
Raghav Joshi <developer.raghavjoshi@gmail.com>
2023-08-23 00:50:50 +0530

    1470

---
+++ leetcode/1470_shuffle.go
@@ -0,0 +1,11 @@
+package main
+
+func Shuffle(nums []int, n int) []int {
+	newArray := []int{}
+
+	for i := 0; i < n; i++{
+		newArray = append(newArray,nums[i], nums[i+n])
+	}
+
+	return newArray
+}
